b41663bd96f2091a04302daec8fd8060de0fab0e
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2025-01-09 00:39:03 +0200

    Update neofetch.lua

--- neofetch.lua
+++ neofetch.lua
@@ -1,4 +1,13 @@
 local gpu = component.proxy(component.list("gpu")())
+
+local function clear()
+    gpu.fill(1, 1, 50, 16, " ")
+end
+
+local function write(x, y, text)
+    gpu.set(x, y, text)
+end
+
 clear()
 write(1, 2, "==============")
 write(1, 3, " = ======== = ")
